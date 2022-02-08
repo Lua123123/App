508ff82db1cd29fb7913fde2e2a8b86bd75e2a6d
nhatanh <hvna2802@gmail.com>
2022-02-09 00:17:57 +0700

    first commit

--- app/src/main/java/com/example/app/Utils/Utils.java
+++ app/src/main/java/com/example/app/Utils/Utils.java
@@ -1,5 +1,5 @@
 package com.example.app.Utils;
 
 public class Utils {
-    public static final String BASE_URL="https://172.27.240.1/banhang/";
+    public static final String BASE_URL="https://192.168.1.18/banhang/";
 }
